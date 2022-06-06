8b14c844c0d5119919f4ba69fe7802b7d0ec2a95
Jonathan Kwon <jonathankwon@yahoo.com>
2022-06-06 13:43:17 -0700

    print fizz, buzz, and fizzbuzz and while loop

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿for (int i = 1; i<=100; i++){
+    bool divisibleByThree = i%3 == 0;
+    bool divisibleByFive = i%5 == 0;
+    if (divisibleByThree && divisibleByFive) {
+        Console.WriteLine("fizzbuzz");
+    } else if (divisibleByFive) {
+        Console.WriteLine("buzz");
+    } else if (divisibleByThree) {
+        Console.WriteLine("fizz");
+    } else {
+        Console.WriteLine(i);
+    }
+}
+
+// int i = 1;
+// while (i < 255) {
+//     i++;
+//     Console.WriteLine(i);
+// }
